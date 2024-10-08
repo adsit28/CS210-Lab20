fa3a754b8106d9fa4353ae578f21a577dc6c0611
adsit28 <c26matthew.adsit@afacademy.af.edu>
2024-10-08 11:48:27 -0600

    done

--- lab20.c
+++ lab20.c
@@ -19,12 +19,12 @@ int main() {
     
     do{
         // UNCOMMENT AFTER COMPLETING TASK 3
-        // minF = getInput("Minimum Fahrenheit temperature: ", -459.67, 10706.0);
-        // maxF = getInput("Maximum Fahrenheit temperature: ", minF, 10706.0);
-        // stepF = getInput("What step size: ", 0, (maxF - minF)/2);
+        minF = getInput("Minimum Fahrenheit temperature: ", -459.67, 10706.0);
+        maxF = getInput("Maximum Fahrenheit temperature: ", minF, 10706.0);
+        stepF = getInput("What step size: ", 0, (maxF - minF)/2);
 
         // UNCOMMENT AFTER COMPLETING TASK 4
-        //printTable(minF, maxF, stepF);
+        printTable(minF, maxF, stepF);
     } while( again() );
     
     return 0;
--- lab20functs.c
+++ lab20functs.c
@@ -11,25 +11,55 @@
 #include "lab20functs.h"
 
 double fToC(double degF){ 
-    return 0.0;
+    return (degF - 32.0)*(5.0/9.0);
 }
 
 double cToK(double degC){ 
-    return 0.0;
+    return degC + 273.15;
 }
 
 void printTable(double degFlow, double degFhigh, double degFstep){
-
+    fprintf(stdout, "\n|--------------------------------|\n");
+    fprintf(stdout, "|                                |\n");
+    fprintf(stdout, "|         Temperature Conversions|\n" );
+    fprintf(stdout, "|                                |\n");
+    fprintf(stdout, "|--------------------------------|\n");
+    fprintf(stdout, "|Fahrenheit|  Celsius |  Kelvin  |\n");
+    fprintf(stdout, "|--------------------------------|\n");
+    while(degFlow <= degFhigh){
+        fprintf(stdout, "|    %10.2lf|    %10.2lf|    %10.2lf|\n", degFlow, fToC(degFlow), cToK(fToC(degFlow)));
+        degFlow = degFlow + degFstep;
+    }
+    fprintf(stdout, "|--------------------------------|\n");
 }
 
 double getInput(char message[], double min, double max) {  
-        
-    return 0.0;
-
+    double userIn = 0.0;
+    bool working = false;
+    fprintf(stdout, "%s", message);
+    fscanf(stdin, "%lf", &userIn);
+    while(working == false){
+        if(userIn < max && userIn > min){
+            working = true;
+        }   
+        else{
+            fprintf(stdout,"Your input is outside of the range <MIN VALUE -- 2 decimal points> to <MAX VALUE -- 2 decimal points>.\nPlease re-enter.");
+            fscanf(stdin, "%lf", &userIn);
+        }
+    }
+    return userIn;
 }
 
 bool again(){
-
-    return false;
-
+    char doAgain = ' ';
+    bool decision;
+    fprintf(stdout, "Would you like to do this again?");
+    fscanf(stdin, " %c", &doAgain);
+    if(doAgain == 'y' || doAgain == 'Y'){
+        decision = true;
+    }
+    else{
+        decision = false;
+    }
+    return decision;
 }
